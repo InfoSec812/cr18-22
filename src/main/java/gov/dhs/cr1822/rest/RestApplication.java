c7b48509917fa2cfd0566b6325babb5120cadc6a
Deven Phillips <deven.phillips@gmail.com>
2018-08-18 12:14:52 -0400

    Initial commit

---
+++ src/main/java/gov/dhs/cr1822/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package gov.dhs.cr1822.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
